bc4e6cebef24d45e413f84c02997ab94c4a298fc
Grace Andrizzi <graceandrizzi@graces-mbp.lan>
2018-09-12 18:07:31 -0700

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+INSERT INTO burgers (name) VALUES ('bbq');
+INSERT INTO burgers (name) VALUES ('hamburger');
+INSERT INTO burgers (name, devoured) VALUES ('aloha', true);
+INSERT INTO burgers (name, devoured) VALUES ('chicken', true);
+INSERT INTO burgers (name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('protein');
